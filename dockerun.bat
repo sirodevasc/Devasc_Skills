13f9f236184f3fa7dec2ea508e557f86e22e8b60
siro <robert.simus@technobel.be>
2020-12-05 21:42:32 +0100

    docker jenking job script creation BAT

---
+++ dockerun.bat
@@ -0,0 +1,14 @@
+echo " "
+echo "pulling image from docker hub..."
+echo " "
+docker pull ntpd/ntpd
+echo " "
+echo " pull finished "
+echo " "
+echo " "
+echo " running docker NTP services... "
+docker run -d --restart=always -p 123:123/udp --name siro ntpd:ntpd
+echo " "
+echo " displaying list of running containers... "
+docker ps -a
+echo " "
